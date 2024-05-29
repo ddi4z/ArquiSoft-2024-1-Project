12a79835f1513595571d43e9bcf9565aa49cccbd
Sara Cárdenas <ss.cardenas@uniandes.edu.co>
2024-05-28 23:34:49 -0500

    bai mdw

--- solicitudes1/solicitudTDC/settings.py
+++ solicitudes1/solicitudTDC/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'logs.loggingMiddleware.LoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'solicitudTDC.urls'
